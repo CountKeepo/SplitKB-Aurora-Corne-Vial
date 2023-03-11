317fecef148b1aeb0346b9c2e00feacf8b85fd6e
CountKeepo <64993772+CountKeepo@users.noreply.github.com>
2023-03-11 19:23:18 +0200

    initial commit

---
+++ keyboards/splitkb/aurora/corne/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE7, 0x31, 0x07, 0xAD, 0x1D, 0x9E, 0x7B, 0xD4}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 3, 7 }
+
+#define VIAL_UNLOCK_COMBO_COLS { 5, 5 }
